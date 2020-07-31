4d02784be7ddc0b82503e2c202ddb55a0c67e932
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-07-31 02:15:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,7 @@
-puts board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  puts row = ["   " "|" "   " "|" "   "]
-  puts separator = "-----------"
-  puts row
-  puts separator
-  puts row
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-puts board = [" "," "," "," ","X"," "," "," "," "]
-def display_board(board)
-  puts row = ["   " "|" "   " "|" "   "]
-  puts separator = "-----------"
-  puts row
-  puts separator
-  puts row
